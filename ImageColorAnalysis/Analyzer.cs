6815918cb0e300031b616f5ca8d964d6da4e553b
Yuriy <yura.ganzin@mail.ru>
2021-12-03 17:57:21 +0300

    Добавил обработку цифр на фото

--- ImageColorAnalysis/Analyzer.cs
+++ ImageColorAnalysis/Analyzer.cs
@@ -1,11 +1,32 @@
 ﻿using ImageColorAnalysis.ImplementationModels;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
+using Tesseract;
 
 namespace ImageColorAnalysis
 {
     public class Analyzer
     {
+        public static void OutputNumInfo(string fileName, string pathToLangFolder)
+        {
+            using (var engine = new TesseractEngine(pathToLangFolder, "eng", EngineMode.Default))
+            {
+                // have to load Pix via a bitmap since Pix doesn't support loading a stream.
+                using (var image = new Bitmap(fileName))
+                {
+                    using (var pix = PixConverter.ToPix(image))
+                    {
+                        using (var page = engine.Process(pix))
+                        {
+                            Console.WriteLine(page.GetMeanConfidence() + " : " + page.GetText());
+                        }
+                    }
+                }
+            }
+        }
+
         public static double GetPercentOfNormalGrains(Dictionary<ScaledRGB, int> colors
             , IRawMaterial materialParameters
             , out int notSuccessColor, out int successColor)
